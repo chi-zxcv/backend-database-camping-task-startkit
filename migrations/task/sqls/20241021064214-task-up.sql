eeb592cc35f5beb236118d423ed8fe702c9746a4
James <Jame472512@gmail.com>
2024-12-01 01:55:13 +0800

    20241201_v3:to 6-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -460,25 +460,36 @@ from
 	"CREDIT_PACKAGE"
 inner join 
 	"CREDIT_PURCHASE" on "CREDIT_PURCHASE".credit_package_id = "CREDIT_PACKAGE".id
+where
+	EXTRACT(MONTH FROM "CREDIT_PURCHASE".created_at)=11
 group by "CREDIT_PACKAGE"."name";
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
 
 		
-select 
+select
 	SUM("銷售價格" * "銷售數量") as "總營收"
 from (
-	select 
+	select
 		"CREDIT_PACKAGE"."name" as "組合包方案名稱",
 		"CREDIT_PACKAGE".price as "銷售價格",
-		COUNT(*) as "銷售數量",
-		"CREDIT_PACKAGE".price * COUNT(*) as "總營收"
-	from 
+		COUNT(*) as "銷售數量"
+	from
 		"CREDIT_PACKAGE"
 	inner join 
 		"CREDIT_PURCHASE" on "CREDIT_PURCHASE".credit_package_id = "CREDIT_PACKAGE".id
+	where
+		EXTRACT(MONTH FROM "CREDIT_PURCHASE".created_at)=11
 	group by "CREDIT_PACKAGE"."name",
 		 	 "CREDIT_PACKAGE".price
 ) AS sales_summary;
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
+select 
+	COUNT(DISTINCT "user_id") AS 預約會員人數
+from 
+	"COURSE_BOOKING"
+where 
+	"COURSE_BOOKING".status in ('即將授課','上課中')
+and 
+	EXTRACT(MONTH FROM "booking_at")=11;
